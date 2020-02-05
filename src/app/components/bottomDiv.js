d77829d19bdc6580d501aa9ad5ca74a5e9c1b47c
Catherine Liahn Yang <liahnee@gmail.com>
2020-02-05 13:58:30 -0800

    linkedin btn

--- src/app/components/bottomDiv.js
+++ src/app/components/bottomDiv.js
@@ -1,34 +1,24 @@
 import React from 'react';
 
+import { Button, Icon } from 'semantic-ui-react';
+
 import BlackBottom from '../assets/b1blckbot.png';
 import BlackTop from '../assets/b1blacktop.png';
 
 
 export default (props) => {
-
-	const bottomStyle = () => {
-		if (props.activeItem === '/about') {
-			return {backgroundColor:'white', backgroundImage:'url('+ BlackTop +')', color:'black'}
-		} else {
-			return {backgroundColor:'black', backgroundImage:`url(${BlackTop})`, color:'white'}
-		}
-	}
-
-	const dividerImg = () => {
-		if (props.activeItem === '/about') {
-			return {backgroundImage:`url(${BlackTop})`}
-		} else {
-			return {backgroundImage:`url(${BlackBottom})`}
-		}
-    }
-    
  return (
      <>
-        <div className='BWdivider' style={{backgroundImage: `url(${BlackBottom})`, backgroundColor:'black', color:'white', minHeight: '20vh'}}>
+        <div className='BWdivider' style={{backgroundImage: `url(${props.activeItem === '/about'? BlackTop : BlackBottom})`, backgroundSize: 'cover', backgroundRepeat:'no-repeat', minHeight: '200px'}}>
         </div>
-        {/* <div className='bottom' style={bottomStyle}>
-            Contact buttons: git, linkedin
-        </div> */}
+        <div className='bottom' style={{backgroundColor: `${props.activeItem === '/about'? 'white' :'black'}`, minHeight: '120px'}}>
+            <div style={{ marginLeft:'auto', marginRight:'auto', paddingTop: '60px', width: '200px'}}>
+            <Button as='a' href='https://www.linkedin.com/in/catherine-yang-0ab40b3a/' target="_blank" color='linkedin' >
+                    <Icon name='linkedin' /> LinkedIn
+                </Button>
+            </div>
+                
+        </div>
     </>
  )
 }
