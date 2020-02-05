62bd94f98bcc26fb8be91703ff90c08abbd52eac
Catherine Liahn Yang <liahnee@gmail.com>
2020-02-05 15:46:12 -0800

    trying to rid mobile chrome scroll bar

--- src/app/components/bottomDiv.js
+++ src/app/components/bottomDiv.js
@@ -5,20 +5,34 @@ import { Button, Icon } from 'semantic-ui-react';
 import BlackBottom from '../assets/b1blckbot.png';
 import BlackTop from '../assets/b1blacktop.png';
 
-
 export default (props) => {
- return (
-     <>
-        <div className='BWdivider' style={{backgroundImage: `url(${props.activeItem === '/about'? BlackTop : BlackBottom})`, backgroundSize: 'cover', backgroundRepeat:'no-repeat', minHeight: '200px'}}>
-        </div>
-        <div className='bottom' style={{backgroundColor: `${props.activeItem === '/about'? 'white' :'black'}`, minHeight: '120px'}}>
-            <div style={{ marginLeft:'auto', marginRight:'auto', paddingTop: '60px', width: '120px'}}>
-            <Button as='a' href='https://www.linkedin.com/in/catherine-yang-0ab40b3a/' target="_blank" color='linkedin' style={{margin: '0'}} >
-                    <Icon name='linkedin' /> LinkedIn
-                </Button>
-            </div>
-                
-        </div>
-    </>
- )
-}
+	return (
+		<React.Fragment>
+			<div
+				className="BWdivider"
+				style={{
+					backgroundImage: `url(${window.location.pathname === '/about' ? BlackTop : BlackBottom})`,
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
+					minHeight: '200px'
+				}}
+			/>
+			<div
+				className="bottom"
+				style={{ backgroundColor: `${window.location.pathname === '/about' ? 'white' : 'black'}`, minHeight: '120px' }}
+			>
+				<div style={{ marginLeft: 'auto', marginRight: 'auto', paddingTop: '60px', width: '120px' }}>
+					<Button
+						as="a"
+						href="https://www.linkedin.com/in/catherine-yang-0ab40b3a/"
+						target="_blank"
+						color="linkedin"
+						style={{ margin: '0' }}
+					>
+						<Icon name="linkedin" /> LinkedIn
+					</Button>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
