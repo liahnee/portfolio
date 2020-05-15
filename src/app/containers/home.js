e7fda84875222ad06d4e4f9dc51621c6cbba31a7
Catherine Liahn Yang <liahnee@gmail.com>
2020-05-15 16:27:08 -0700

    fixed typo

--- src/app/containers/home.js
+++ src/app/containers/home.js
@@ -28,52 +28,9 @@ export default (props) => {
 				</Header>
 				<p id="home-intro" >
 					I enjoy being creative with Javascript and oil paints. <br />
-					(Separetely!)
+					(Separately!)
 				</p>
 			</div>
-
-
-				{/* {isMobile ? (
-					<React.Fragment id="">
-						<Image circular size="medium" src={ProfilePic} />
-						<Header
-							// id="home-header"
-							textAlign="left"
-							style={{
-								paddingTop: '10%',
-								fontFamily:  "'Patua One', cursive",
-								fontSize: '40px'
-							}}
-						>
-							Hello, I'm <br /> Catherine Yang.< br/>
-						</Header>
-						<p id="home-intro" style={{ textAlign:'left', fontFamily: "'Merienda', cursive", fontSize: '3.5rem', paddingTop: '30px' }}>
-							I enjoy being creative with Javascript and oil paints. <br />
-							(Separetely!)
-						</p>
-					</React.Fragment>
-				) : (
-					<React.Fragment>
-						<Image circular floated="left" size="medium" src={ProfilePic} />
-						<Header
-							// id="home-header"
-							textAlign="left"
-							style={{
-								paddingTop: '10%',
-								paddingLeft: '350px',
-								fontFamily: "'Patua One', cursive",
-								fontSize: '5rem'
-							}}
-						>
-							Hello, <br />
-							I'm Catherine Yang.
-						</Header>
-						<p id="home-intro" style={{ fontFamily: "'Merienda', cursive", fontSize: '2rem'}}>
-							I enjoy being creative with Javascript and oil paints. <br />
-							(Separately!)
-						</p>
-					</React.Fragment>
-				)} */}
 		</div>
 	);
 };
